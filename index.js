7c3bece255410af68dd77d404515cde7a8021b7b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-10-27 23:37:13 -0400

    Plugs metadata

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict'
 
+const { $dataProvider } = require('./symbols')
+const metadata = require('./metadata')
 const handlers = {}
 
-handlers.GET = async ({ redirect, request, response }) => {
+handlers.GET = async ({ mapping, redirect, request, response }) => {
+  if (redirect.startsWith('$metadata')) {
+    metadata(...arguments)
+  }
 }
 
 handlers.POST = async ({ redirect, request, response }) => {
@@ -15,6 +20,11 @@ module.exports = {
   async validate (mapping) {
   },
   method: Object.keys(handlers),
-  async redirect ({ mapping, match, redirect, request, response }) {
+  async redirect ({ mapping, redirect, request, response }) {
+    if (!mapping[$dataProvider]) {
+      mapping[$dataProvider] = mapping['data-provider-factory']()
+      // check interface
+    }
+    return handlers[request.method](...arguments)
   }
 }

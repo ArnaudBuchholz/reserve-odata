c3eb76427ec4c53420e81b6dcd4e36cf7dd1560b
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-10-27 23:38:04 -0400

    test Record class

---
+++ tests/mocha/Record.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const gpf = require('gpf-js')
+const attribute = gpf.attributes.decorator
+const Searchable = require('../../attributes/Searchable')
+const Sortable = require('../../attributes/Sortable')
+
+const minDate = new Date(0)
+
+class Record {
+  get id () {
+    return this._id
+  }
+
+  get name () {
+    return this._name || ''
+  }
+
+  get number () {
+    return this._number || 0
+  }
+
+  get modified () {
+    return this._modified || minDate
+  }
+}
+
+attribute(new Id())(Record, 'id')
+attribute(new gpf.attributes.Serializable())(Record, 'id')
+attribute(new gpf.attributes.Serializable())(Record, 'type')
+attribute(new gpf.attributes.Serializable())(Record, 'name')
+attribute(new Searchable())(Record, 'name')
+attribute(new Sortable())(Record, 'name')
+attribute(new gpf.attributes.Serializable())(Record, 'number')
+attribute(new gpf.attributes.Serializable({ type: gpf.serial.types.datetime, readOnly: false }))(Record, 'modified')
+attribute(new Sortable())(Record, 'modified')
+
+module.exports = Record

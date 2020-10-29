997167f6478d9d50ab3dbbe9ce7f53295dffdc81
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-10-28 22:54:56 -0400

    Display XML first (for the moment)

--- tests/mocha/metadata.test.js
+++ tests/mocha/metadata.test.js
@@ -4,12 +4,11 @@ const assert = require('assert')
 const handle = require('./handle.js')
 
 describe('metadata', () => {
-
   it('provides entities description', handle({ request: '$metadata' })
     .then(response => {
-      assert.strictEqual(response.statusCode, 200)
       const xml = response.toString()
-      console.log(xml)
+      console.log(`$metadata: ${xml}`)
+      assert.strictEqual(response.statusCode, 200)
     })
   )
 })

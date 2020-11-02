9c531f68afd7c0311be4c32f81de7bb1ea63369d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-11-01 23:42:14 -0500

    DataProviderClasses

--- tests/mocha/handle.js
+++ tests/mocha/handle.js
@@ -7,20 +7,15 @@ const handler = require('../../index')
 
 const Record = require('./Record')
 
-module.exports = function ({ request }) {
+module.exports = function ({ request, classes = [Record] }) {
   if (typeof request === 'string') {
     request = { method: 'GET', url: request }
   }
   request = new Request(request)
   const response = new Response()
   const configuration = { handler: () => { return { handler } } }
-  const dataProvider = {
-    async getEntityClasses () {
-      return [Record]
-    }
-  }
   const mapping = {
-    'data-provider-factory': () => dataProvider,
+    'data-provider-classes': () => classes,
     'service-namespace': 'test'
   }
   return check(configuration, mapping)

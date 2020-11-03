dfed5767b9600c961b6814e4d878371436d749f5
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-11-02 22:56:43 -0500

    Working on Record

--- tests/mocha/Record.js
+++ tests/mocha/Record.js
@@ -5,12 +5,18 @@ const attribute = gpf.attributes.decorator
 const Key = require('../../attributes/Key')
 const Searchable = require('../../attributes/Searchable')
 const Sortable = require('../../attributes/Sortable')
+const NavigationProperty = require('../../attributes/NavigationProperty')
+const Tag = require('./Tag')
 
 const minDate = new Date(0)
 
 class Record {
   get id () {
     return this._id
+  }
+
+  get parentId () {
+    return this._parentId
   }
 
   get name () {
@@ -24,15 +30,28 @@ class Record {
   get modified () {
     return this._modified || minDate
   }
+
+  getChildren () {
+  }
+
+  getParent () {
+  }
+
+  getTags () {
+  }
 }
 
-attribute(new Key())(Record, 'id')
 attribute(new gpf.attributes.Serializable())(Record, 'id')
+attribute(new Key())(Record, 'id')
+attribute(new gpf.attributes.Serializable())(Record, 'parentId')
 attribute(new gpf.attributes.Serializable())(Record, 'name')
 attribute(new Searchable())(Record, 'name')
 attribute(new Sortable())(Record, 'name')
 attribute(new gpf.attributes.Serializable({ type: gpf.serial.types.integer, readOnly: false }))(Record, 'number')
 attribute(new gpf.attributes.Serializable({ type: gpf.serial.types.datetime, readOnly: false }))(Record, 'modified')
 attribute(new Sortable())(Record, 'modified')
+attribute(new NavigationProperty('children', Record, '*').on({ id: 'parentId' }))(Record, 'getChildren')
+attribute(new NavigationProperty('parent', Record, 1).on({ parentId: 'id' }))(Record, 'getParent')
+attribute(new NavigationProperty('tags', Tag, '*'))(Record, 'buildContent')
 
 module.exports = Record

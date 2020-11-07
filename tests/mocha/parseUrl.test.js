9bf3ddd1ad7856b37d1e6afab0a8fce8efc94e9d
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-11-07 00:10:24 -0500

    Parsing URL

---
+++ tests/mocha/parseUrl.test.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const assert = require('assert')
+const parseUrl = require('../../parseUrl')
+
+const scenarios = {
+  'RecordSet': {
+    set: 'RecordSet'
+  },
+  'RecordSet(\'abc\')': {
+    set: 'RecordSet',
+    key: 'abc'
+  },
+  'RecordSet(\'abc\')': {
+    set: 'RecordSet',
+    key: 'abc'
+  },
+  'RecordSet("abc")': {
+    set: 'RecordSet',
+    key: 'abc'
+  },
+  'AppSetting(application="Example",version=1,setting="Preview")': {
+    set: 'AppSetting',
+    key: {
+      application: 'Example',
+      version: '1',
+      setting: 'Preview'
+    }
+  }
+}
+
+describe('parseUrl', () => {
+  Object.keys(scenarios).forEach(url => it(url, () => {
+    assert.deepStrictEqual(parseUrl(url), scenarios[url])
+  }))
+})

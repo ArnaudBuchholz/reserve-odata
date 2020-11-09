d816c67da844b7b1b309d65e1e6750e9912ae56f
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-11-09 00:15:04 -0500

    Testing parsing

--- tests/mocha/parseUrl.test.js
+++ tests/mocha/parseUrl.test.js
@@ -22,6 +22,65 @@ const scenarios = {
       version: '1',
       setting: 'Preview'
     }
+  },
+  'RecordSet("123")/Tags': {
+    set: 'RecordSet',
+    key: '123',
+    navigationProperties: ['Tags']
+  },
+  'RecordSet("456")/Tags/Records': {
+    set: 'RecordSet',
+    key: '456',
+    navigationProperties: ['Tags', 'Records']
+  },
+  'RecordSet?$top=10&$skip=0': {
+    set: 'RecordSet',
+    parameters: {
+      $skip: 0,
+      $top: 10
+    }
+  },
+  'RecordSet?$skip=0&$top=10': {
+    set: 'RecordSet',
+    parameters: {
+      $skip: 0,
+      $top: 10
+    }
+  },
+  'RecordSet?$skip=123&$orderby=Rating asc,Priority,Category/Name desc&$top=456': {
+    set: 'RecordSet',
+    parameters: {
+      $skip: 123,
+      $top: 456,
+      $orderby: {
+        Rating: true,
+        Priority: true,
+        'Category/Name': false
+      }
+    }
+  },
+  'RecordSet?$expand=tags,parent,children': {
+    set: 'RecordSet',
+    parameters: {
+      $expand: ['tags', 'parent', 'children']
+    }
+  },
+  'RecordSet?$select=Name,Count': {
+    set: 'RecordSet',
+    parameters: {
+      $select: ['Name', 'Count']
+    }
+  },
+  'RecordSet?$filter=Count eq 2': {
+    set: 'RecordSet',
+    parameters: {
+      $filter: {
+        eq: [
+          { property: 'Count' },
+          2
+        ]
+      }
+    }
   }
 }
 

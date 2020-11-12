247214b51f62609c293986304ee5c8806dff3cde
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-11-11 23:26:21 -0500

    Test parsing

--- tests/mocha/parseUrl.test.js
+++ tests/mocha/parseUrl.test.js
@@ -11,26 +11,22 @@ const scenarios = {
     set: 'RecordSet',
     key: 'abc'
   },
-  'RecordSet("abc")': {
-    set: 'RecordSet',
-    key: 'abc'
-  },
-  'AppSetting(application="Example",version=1,setting=\'Preview\')': {
+  'AppSetting(application=\'Example\',version=1,setting=\'Preview\')': {
     set: 'AppSetting',
     key: {
       application: 'Example',
-      version: '1',
+      version: 1,
       setting: 'Preview'
     }
   },
-  'RecordSet("123")/Tags': {
+  'RecordSet(\'123\')/Tags': {
     set: 'RecordSet',
     key: '123',
     navigationProperties: ['Tags']
   },
-  'RecordSet("456")/Tags/Records': {
+  'RecordSet(456)/Tags/Records': {
     set: 'RecordSet',
-    key: '456',
+    key: 456,
     navigationProperties: ['Tags', 'Records']
   },
   'RecordSet?$top=10&$skip=0': {
@@ -79,6 +75,108 @@ const scenarios = {
           { property: 'Count' },
           2
         ]
+      }
+    }
+  },
+  'Suppliers?$filter=Address/City eq \'Redmond\'': {
+    set: 'Suppliers',
+    parameters: {
+      $filter: {
+        eq: [
+          { property: 'Address/City' },
+          'Redmond'
+        ]
+      }
+    }
+  },
+  'Suppliers?$filter=Address/City ne \'London\'': {
+    set: 'Suppliers',
+    parameters: {
+      $filter: {
+        ne: [
+          { property: 'Address/City' },
+          'London'
+        ]
+      }
+    }
+  },
+  'Products?$filter=Price gt 20': {
+    set: 'Products',
+    parameters: {
+      $filter: {
+        gt: [
+          { property: 'Price' },
+          20
+        ]
+      }
+    }
+  },
+  'Products?$filter=Price ge 10': {
+    set: 'Products',
+    parameters: {
+      $filter: {
+        gte: [
+          { property: 'Price' },
+          10
+        ]
+      }
+    }
+  },
+  'Products?$filter=Price lt 20': {
+    set: 'Products',
+    parameters: {
+      $filter: {
+        lt: [
+          { property: 'Price' },
+          20
+        ]
+      }
+    }
+  },
+  'Products?$filter=Price le 100': {
+    set: 'Products',
+    parameters: {
+      $filter: {
+        lte: [
+          { property: 'Price' },
+          100
+        ]
+      }
+    }
+  },
+  'Products?$filter=Price le 200 and Price gt 3.5': {
+    set: 'Products',
+    parameters: {
+      $filter: {
+        and: [{
+          lte: [
+            { property: 'Price' },
+            200
+          ]
+        }, {
+          gt: [
+            { property: 'Price' },
+            3.5
+          ]
+        }]
+      }
+    }
+  },
+  'Products?$filter=Price le 3.5 or Price gt 200  ': {
+    set: 'Products',
+    parameters: {
+      $filter: {
+        or: [{
+          lte: [
+            { property: 'Price' },
+            3.5
+          ]
+        }, {
+          gt: [
+            { property: 'Price' },
+            200
+          ]
+        }]
       }
     }
   }

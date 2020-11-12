a4b6c3e39573c6b12931dac21dc4383c0061e822
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-11-11 23:26:31 -0500

    Implementation of parsing

--- parseUrl.js
+++ parseUrl.js
@@ -1,35 +1,48 @@
 'use strict'
 
+const toNumber = value => {
+  if (value.includes('.')) {
+    return parseFloat(value)
+  }
+  return parseInt(value, 10)
+}
+
 const parseKey = key => {
-  const match = /^(?:'([^']*)'|"([^"]*)"|(.*))$/.exec(key)
+  const match = /^(?:'([^']*)'|(\d+)|(.*))$/.exec(key)
   if (match[3]) {
     const keys = {}
-    match[3].replace(/([^=]+)=(?:"([^"]+)"|'([^']+)'|([^,]+))(?:,|$)/g, function (_, field, value1, value2, value3) {
-      keys[field] = value1 || value2 || value3
+    match[3].replace(/([^=]+)=(?:'([^']+)'|([^,]+))(?:,|$)/g, function (_, field, value1, value2) {
+      keys[field] = value1 || toNumber(value2)
     })
     return keys
   }
-  return match[1] || match[2]
+  return match[1] || toNumber(match[2])
 }
 
-const toNumber = value => parseInt(value, 10)
 const toList = value => value.split(',')
 
 function parseFilter (string) {
   const tokens = []
-  string.replace(/\(|\)|\d+|[\w0-9_]+/g, token => tokens.push(token))
+  string.replace(/\(|\)|\d+(\.\d*)?|'[^']+'|[\w0-9_]+(\/[\w0-9_]+)*/g, token => tokens.push(token))
 
   const invalidFilter = () => { throw new Error('invalid filter') }
 
-  // filter -> or_cond or or_cond
-  // or_cond -> and_cond and and_cond
-  // and_cond -> field operator value
+  const operatorMapping = {
+    ge: 'gte',
+    le: 'lte'
+  }
 
   function parseCondition () {
     const [property, operator, rawValue] = tokens
     tokens.splice(0, 3)
-    if (operator === 'eq') {
-      return { [operator]: [{ property }, parseInt(rawValue, 10)]}
+    let value
+    if (rawValue.startsWith('\'')) {
+      value = rawValue.substring(1, rawValue.length - 1)
+    } else {
+      value = toNumber(rawValue)
+    }
+    if (['eq', 'ne', 'gt', 'ge', 'lt', 'le'].includes(operator)) {
+      return { [operatorMapping[operator] || operator]: [{ property }, value] }
     }
     invalidFilter()
   }
@@ -45,11 +58,8 @@ function parseFilter (string) {
       } else {
         filter = { [operator]: [filter, condition] }
       }
-      if (!tokens.length) {
+      if (!tokens.length || tokens[0] !== operator) {
         break
-      }
-      if (tokens[0] !== operator) {
-        invalidFilter()
       }
       tokens.shift()
       if (!tokens.length) {

985c31718b2a9c7d2e3ab8b888d9ec6a7af38655
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-11-15 09:46:42 -0500

    Enables entity name and setName configuration

---
+++ attributes/Entity.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const gpf = require('gpf-js')
+const attribute = gpf.attributes.decorator
+
+class Entity extends gpf.attributes.Attribute {
+  get name () {
+    return this._name
+  }
+
+  get set () {
+    return this._setName
+  }
+
+  constructor (name, setName = `${name}Set`) {
+    super()
+    this._name = name
+    this._setName = setName
+  }
+}
+
+attribute(new gpf.attributes.ClassAttribute())(Entity)
+
+Entity.get = EntityClass => {
+  const dictionary = gpf.attributes.get(EntityClass, Entity)
+  if (dictionary.$attributes) {
+    return dictionary.$attributes[0]
+  }
+}
+
+module.exports = Entity

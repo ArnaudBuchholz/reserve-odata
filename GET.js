5f66a92d23c62511acf49c1d9daf5a3782b29ac2
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-11-21 00:17:57 -0500

    Linter fix

--- GET.js
+++ GET.js
@@ -16,51 +16,51 @@ function getNavigationPropertyMethodName (entity, navigationPropertyName) {
 }
 
 module.exports = async function ({ mapping, redirect, request, response }) {
-    if (redirect.startsWith('$metadata')) {
-      return metadata(...arguments)
+  if (redirect.startsWith('$metadata')) {
+    return metadata(...arguments)
+  }
+  const parsedUrl = parseUrl(redirect)
+  const EntityClass = mapping[$set2dpc][parsedUrl.set]
+  let entities
+  if (parsedUrl.key) {
+    entities = [await Entity.read(EntityClass, request, parsedUrl.key)]
+    if (parsedUrl.navigationProperties) {
+      entities = parsedUrl.navigationProperties.reduce((context, navigationPropertyName, index) => {
+        let filter
+        if (index === parsedUrl.navigationProperties.length - 1) {
+          filter = parsedUrl.parameters.$filter
+        }
+        const memberName = getNavigationPropertyMethodName(context[0], navigationPropertyName) // Assuming same type for all
+        return context.reduce((result, entity) => result.concat(entity[memberName](filter)), [])
+      }, entities)
     }
-    const parsedUrl = parseUrl(redirect)
-    const EntityClass = mapping[$set2dpc][parsedUrl.set]
-    let entities
-    if (parsedUrl.key) {
-      entities = [await Entity.read(EntityClass, request, parsedUrl.key)]
-      if (parsedUrl.navigationProperties) {
-        entities = parsedUrl.navigationProperties.reduce((context, navigationPropertyName, index) => {
-          let filter
-          if (index === parsedUrl.navigationProperties.length - 1) {
-            filter = parsedUrl.parameters.$filter
-          }
-          const memberName = getNavigationPropertyMethodName(context[0], navigationPropertyName) // Assuming same type for all
-          return context.reduce((result, entity) => result.concat(entity[memberName](filter)), [])
-        }, entities)
-      }
-    } else {
-  
-    }
-    // expand
-    if (parsedUrl.parameters.$expand) {
-        parsedUrl.parameters.$expand.forEach(navigationPropertyName => {
-          const memberName = getNavigationPropertyMethodName(entities[0], navigationPropertyName) // Assuming same type for all
-          entities.forEach(entity => {
-            entity[navigationPropertyName] = entity[memberName]()
-          })
-        })
-    }
-    entities = entities.map(entity => toJSON(entity, mapping['service-namespace'], parsedUrl.parameters.$select))
-    let result
-    if (parsedUrl.key && !parsedUrl.navigationProperties) {
-      result = entities[0]
-    } else {
-      result = { results: entities }
-      // $order
-      // $top && $skip
-    }
-    const content = JSON.stringify({
-      d: result
+  } else {
+
+  }
+  // expand
+  if (parsedUrl.parameters.$expand) {
+    parsedUrl.parameters.$expand.forEach(navigationPropertyName => {
+      const memberName = getNavigationPropertyMethodName(entities[0], navigationPropertyName) // Assuming same type for all
+      entities.forEach(entity => {
+        entity[navigationPropertyName] = entity[memberName]()
+      })
     })
-    response.writeHead(200, {
-      'Content-Type': jsonContentType,
-      'Content-Length': content.length
-    })
-    response.end(content)
+  }
+  entities = entities.map(entity => toJSON(entity, mapping['service-namespace'], parsedUrl.parameters.$select))
+  let result
+  if (parsedUrl.key && !parsedUrl.navigationProperties) {
+    result = entities[0]
+  } else {
+    result = { results: entities }
+    // $order
+    // $top && $skip
+  }
+  const content = JSON.stringify({
+    d: result
+  })
+  response.writeHead(200, {
+    'Content-Type': jsonContentType,
+    'Content-Length': content.length
+  })
+  response.end(content)
 }

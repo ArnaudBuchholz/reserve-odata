5e143648cd4e42be5767c2dc37884066cd7dee68
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-12-01 07:56:47 -0500

    Isolate helpers

---
+++ util.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const gpf = require('gpf-js')
+
+const getNamedProperties = entity => {
+  const memberProperties = gpf.serial.get(entity)
+  return Object.keys(memberProperties).reduce((dictionary, member) => {
+    const property = memberProperties[member]
+    property.member = member
+    dictionary[property.name] = property
+    return dictionary
+  }, {})
+}
+
+const mapFilterProperties = (filter, EntityClass) => {
+  const namedProperties = getNamedProperties(EntityClass)
+  function map (filterItem) {
+    if (filterItem.property) {
+      filterItem.property = namedProperties[filterItem.property].member
+    }
+    Object.keys(filterItem).forEach(property => {
+      const value = filterItem[property]
+      if (Array.isArray(value)) {
+        value.forEach(map)
+      } else if (typeof value === 'object') {
+        map(value)
+      }
+    })
+  }
+  if (filter) {
+    map(filter)
+  }
+  return filter
+}
+
+module.exports = {
+  getNamedProperties,
+  mapFilterProperties
+}

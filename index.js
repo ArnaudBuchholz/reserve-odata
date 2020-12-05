fb0aef87bf67513dbd8e758fe877e64d0a30f3ed
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-12-04 23:18:02 -0500

    Validate mapping

--- index.js
+++ index.js
@@ -11,18 +11,32 @@ handlers.DELETE = async function ({ cache, redirect, response }) {
 }
 
 module.exports = {
+  schema: {
+    [$dpc]: ['function', 'string'],
+    'use-sap-extension': {
+      type: 'boolean',
+      defaultValue: false
+    },
+    'service-namespace': {
+      type: 'string',
+      defaultValue: 'ODATANS'
+    }
+  },
   async validate (mapping) {
+    const dpc = mapping[$dpc]
+    if (typeof dpc === 'string') {
+      mapping[$dpc] = await require(dpc)()
+    } else {
+      mapping[$dpc] = await mapping['data-provider-classes']()
+    }
+    mapping[$set2dpc] = mapping[$dpc].reduce((mapping, EntityClass) => {
+      const { setName } = Entity.names(EntityClass)
+      mapping[setName] = EntityClass
+      return mapping
+    }, {})
   },
   method: Object.keys(handlers),
   async redirect ({ mapping, request, response }) {
-    if (!mapping[$dpc]) {
-      mapping[$dpc] = await mapping['data-provider-classes']()
-      mapping[$set2dpc] = mapping[$dpc].reduce((mapping, EntityClass) => {
-        const { setName } = Entity.names(EntityClass)
-        mapping[setName] = EntityClass
-        return mapping
-      }, {})
-    }
     try {
       return await handlers[request.method](...arguments)
     } catch (e) {

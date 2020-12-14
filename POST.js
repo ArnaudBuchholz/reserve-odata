10827e839e8ed9412b304e993e13dd71a6037524
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-12-13 23:53:30 -0500

    Simpler

--- POST.js
+++ POST.js
@@ -1,20 +1,11 @@
 'use strict'
 
-const { $set2dpc } = require('./symbols')
-const Entity = require('./attributes/Entity')
+const { create } = require('./entity')
 const { body } = require('reserve')
 const toJSON = require('./toJSON')
 
-module.exports = async function ({ mapping, parsedUrl, request, response }) {
-  if (parsedUrl.owns$parameter) {
-    throw new Error('Unsupported parameter')
-  }
-  const EntityClass = mapping[$set2dpc][parsedUrl.set]
-  if (!EntityClass) {
-    throw new Error('Unkown entity class')
-  }
-  const definition = JSON.parse(await body(request))
-  const entity = Entity.create(EntityClass, request, definition)
+module.exports = async function ({ EntityClass, mapping, parsedUrl, request, response }) {
+  const entity = await create(EntityClass, request, JSON.parse(await body(request)))
   const d = toJSON(entity, mapping['service-namespace'])
   const content = JSON.stringify({ d })
   response.writeHead(201, {

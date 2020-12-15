426bd9993d79377e169b7d620204a3c6bf3d8728
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-12-15 07:48:24 -0500

    Check if entity exists

--- PUT.js
+++ PUT.js
@@ -1,13 +1,18 @@
 'use strict'
 
-const { update } = require('./entity')
+const { get, update } = require('./entity')
 const { body } = require('reserve')
 
 module.exports = async function ({ EntityClass, parsedUrl, request, response }) {
-  if (!parsedUrl.key) {
+  const { key } = parsedUrl
+  if (!key) {
     throw new Error('Missing key')
   }
-  await update(EntityClass, request, parsedUrl.key, JSON.parse(await body(request)))
+  const entity = await get(EntityClass, request, key)
+  if (!entity) {
+    throw new Error('Entity not found')
+  }
+  await update(EntityClass, request, key, JSON.parse(await body(request)))
   response.writeHead(204, {
     'Content-Type': 'application/json',
     'Content-Length': 0

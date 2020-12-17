d1946af87b4eb679e5199f234088fd67c1a9d719
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-12-17 06:15:59 -0500

    PUT

--- PUT.js
+++ PUT.js
@@ -1,21 +1,5 @@
 'use strict'
 
-const { get, update } = require('./entity')
-const { body } = require('reserve')
+const update = require('./update')
 
-module.exports = async function ({ EntityClass, parsedUrl, request, response }) {
-  const { key } = parsedUrl
-  if (!key) {
-    throw new Error('Missing key')
-  }
-  const entity = await get(EntityClass, request, key)
-  if (!entity) {
-    throw new Error('Entity not found')
-  }
-  await update(EntityClass, request, key, JSON.parse(await body(request)))
-  response.writeHead(204, {
-    'Content-Type': 'application/json',
-    'Content-Length': 0
-  })
-  response.end()
-}
+module.exports = update((entity, jsonBody) => jsonBody)

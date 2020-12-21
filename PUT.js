80937dfc10c1a403aa48d81480c6e554b78d90ca
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-12-21 17:26:53 -0500

    Ensures that only updates are transmitted

--- PUT.js
+++ PUT.js
@@ -6,7 +6,20 @@ const update = require('./update')
 module.exports = update((entity, properties) => {
   const memberProperties = gpf.serial.get(entity)
   Object.keys(memberProperties).forEach(member => {
-    if (!Object.hasOwnProperty.call(properties, member)) {
+    const { type } = memberProperties[member]
+    if (Object.hasOwnProperty.call(properties, member)) {
+      const newValue = properties[member]
+      const oldValue = entity[member]
+      let diff
+      if (type === gpf.serial.types.datetime) {
+        diff = newValue.getTime() !== oldValue.getTime()
+      } else {
+        diff = newValue !== oldValue
+      }
+      if (!diff) {
+        delete properties[member]
+      }
+    } else {
       properties[member] = undefined // delete
     }
   })

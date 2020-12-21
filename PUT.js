58c6b60bee591be1810fac5c96fd11dd505e2869
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-12-21 17:04:21 -0500

    Signal missing properties

--- PUT.js
+++ PUT.js
@@ -1,5 +1,14 @@
 'use strict'
 
+const gpf = require('gpf-js')
 const update = require('./update')
 
-module.exports = update((entity, jsonBody) => jsonBody)
+module.exports = update((entity, properties) => {
+  const memberProperties = gpf.serial.get(entity)
+  Object.keys(memberProperties).forEach(member => {
+    if (!Object.hasOwnProperty.call(properties, member)) {
+      properties[member] = undefined // delete
+    }
+  })
+  return properties
+})

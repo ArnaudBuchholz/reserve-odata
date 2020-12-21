35c09e5439f33f18b6629e487107aad27a2d5065
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-12-20 23:51:47 -0500

    Testing update

---
+++ tests/mocha/update.test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const assert = require('assert')
+const { handle, test, reset, fail } = require('./handle.js')
+
+describe('update', () => {
+  describe('PUT', () => {
+    before(reset)
+
+    test('PUT', '/Record(\'abc\')', {
+      name: 'updated',
+      number: 2748,
+      //  modified
+    }, async response => {
+      assert.strictEqual(response.statusCode, 204)
+      const entity = JSON.parse(response.toString())
+      assert.strictEqual(entity.name, 'update')
+      assert.strictEqual(entity.number, 2478)
+      assert.strictEqual(entity.modified, undefined)
+      const readResponse = await handle({ request: 'RecordSet(\'abc\')' })
+      assert.strictEqual(readResponse.statusCode, 200)
+      const readEntity = JSON.parse(readResponse.toString())
+      assert.strictEqual(readEntity.name, 'update')
+      assert.strictEqual(readEntity.number, 2478)
+      assert.strictEqual(readEntity.modified, undefined)
+    })
+  })
+
+  describe('MERGE', () => {
+    before(reset)
+
+  })
+})

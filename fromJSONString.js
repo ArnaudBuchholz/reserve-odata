751fb95866012774d1766fc3405a476299279600
ArnaudBuchholz <arnaud.buchholz@free.fr>
2020-12-22 23:45:06 -0500

    Also check value type

--- fromJSONString.js
+++ fromJSONString.js
@@ -2,6 +2,28 @@
 
 const gpf = require('gpf-js')
 
+const mapOfJSONToSerialType = {}
+
+const invalidType = () => { throw new Error('Invalid type') }
+
+const checkType = type => value => {
+  if (typeof value !== type) {
+    invalidType()
+  }
+  return value
+}
+
+mapOfJSONToSerialType[gpf.serial.types.integer] = checkType('number')
+mapOfJSONToSerialType[gpf.serial.types.string] = checkType('string')
+mapOfJSONToSerialType[gpf.serial.types.datetime] = value => {
+  try {
+    const timeDigits = /\/Date\((\d+)\)\//.exec(value)[1]
+    return new Date(parseInt(timeDigits, 10))
+  } catch (e) {
+    invalidType()
+  }
+}
+
 module.exports = function fromJSONString (EntityClass, string) {
   const json = JSON.parse(string)
   const entity = {}
@@ -10,11 +32,10 @@ module.exports = function fromJSONString (EntityClass, string) {
     const { name, type } = memberProperties[member]
     if (Object.hasOwnProperty.call(json, name)) {
       const value = json[name]
-      if (type === gpf.serial.types.datetime) {
-        const timeDigits = /\/Date\((\d+)\)\//.exec(value)[1]
-        entity[member] = new Date(parseInt(timeDigits, 10))
+      if (value === null) {
+        entity[member] = null
       } else {
-        entity[member] = value
+        entity[member] = mapOfJSONToSerialType[type](value)
       }
     }
   })

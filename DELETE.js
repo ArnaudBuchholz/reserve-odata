9a7540cd16fefa9a1e1cbd998c51ad7f16561328
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-01-02 00:16:24 -0500

    Changes delete helper (requires entity)

--- DELETE.js
+++ DELETE.js
@@ -1,15 +1,14 @@
 'use strict'
 
-const { delete: delete_ } = require('./entity')
+const { getOrFail, delete: delete_ } = require('./entity')
 
 module.exports = async function ({ EntityClass, mapping, parsedUrl, request, response }) {
-  if (!parsedUrl.key) {
+  const { key } = parsedUrl
+  if (!key) {
     throw new Error('Missing key')
   }
-  const deleted = await delete_(EntityClass, request, parsedUrl.key)
-  if (!deleted) {
-    throw new Error('Not deleted')
-  }
+  const entity = await getOrFail(EntityClass, request, key)
+  await delete_(EntityClass, request, entity)
   response.writeHead(204)
   response.end()
 }

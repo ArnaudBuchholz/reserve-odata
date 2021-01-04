f99d8be2ef01cb3530ac4091823ef8a19d144c74
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-01-03 23:31:45 -0500

    Removes Searchable (no value)

--- tests/Tag.js
+++ tests/Tag.js
@@ -3,7 +3,6 @@
 const gpf = require('gpf-js')
 const attribute = gpf.attributes.decorator
 const Key = require('../attributes/Key')
-const Searchable = require('../attributes/Searchable')
 const Sortable = require('../attributes/Sortable')
 
 class Tag {
@@ -27,7 +26,6 @@ class Tag {
 
 attribute(new gpf.attributes.Serializable())(Tag, 'name')
 attribute(new Key())(Tag, 'name')
-attribute(new Searchable())(Tag, 'name')
 attribute(new gpf.attributes.Serializable({ type: gpf.serial.types.integer, readOnly: false }))(Tag, 'count')
 attribute(new gpf.attributes.Serializable({ type: gpf.serial.types.datetime, readOnly: false }))(Tag, 'modified')
 attribute(new Sortable())(Tag, 'modified')

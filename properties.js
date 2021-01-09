848a6a51f0ed42a8270b379db56cc984b7669f66
ArnaudBuchholz <arnaud.buchholz@free.fr>
2021-01-08 23:56:43 -0500

    Reduces code complexity

--- properties.js
+++ properties.js
@@ -5,7 +5,7 @@ const Key = require('./attributes/Key')
 const Filterable = require('./attributes/Filterable')
 const Sortable = require('./attributes/Sortable')
 
-const getNamedProperties = entity => {
+function getNamedProperties (entity) {
   const memberProperties = gpf.serial.get(entity)
   return Object.keys(memberProperties).reduce((dictionary, member) => {
     const property = memberProperties[member]
@@ -15,30 +15,31 @@ const getNamedProperties = entity => {
   }, {})
 }
 
-const mapFilterProperties = (filter, EntityClass) => {
+function mapFilterPropertiesOnItem (namedProperties, filterable, filterItem) {
+  if (filterItem.property) {
+    filterItem.property = namedProperties[filterItem.property].member
+    if (!filterable.includes(filterItem.property)) {
+      throw new Error('Filtering on non Filterable property')
+    }
+  }
+  Object.keys(filterItem).forEach(property => {
+    const value = filterItem[property]
+    if (Array.isArray(value)) {
+      value.forEach(mapFilterPropertiesOnItem.bind(null, namedProperties, filterable))
+    }
+  })
+}
+
+function mapFilterProperties (filter, EntityClass) {
   const namedProperties = getNamedProperties(EntityClass)
   const filterable = Object.keys(gpf.attributes.get(EntityClass, Key)).concat(Object.keys(gpf.attributes.get(EntityClass, Filterable)))
-  function map (filterItem) {
-    if (filterItem.property) {
-      filterItem.property = namedProperties[filterItem.property].member
-      if (!filterable.includes(filterItem.property)) {
-        throw new Error('Filtering on non Filterable property')
-      }
-    }
-    Object.keys(filterItem).forEach(property => {
-      const value = filterItem[property]
-      if (Array.isArray(value)) {
-        value.forEach(map)
-      }
-    })
-  }
   if (filter) {
-    map(filter)
+    mapFilterPropertiesOnItem(namedProperties, filterable, filter)
   }
   return filter
 }
 
-const mapOrderByProperties = (orderby, EntityClass) => {
+function mapOrderByProperties (orderby, EntityClass) {
   const namedProperties = getNamedProperties(EntityClass)
   const sortable = Object.keys(gpf.attributes.get(EntityClass, Sortable))
   orderby.forEach(orderItem => {
